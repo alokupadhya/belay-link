da5c490b88273e6840ce9e77ac76513e4c380af7
alokupadhya <alok.upadhya0@gmail.com>
2023-06-26 23:12:00 +0530

    Updated: Fix font, spacing issue

--- src/App/Containers/Home/index.js
+++ src/App/Containers/Home/index.js
@@ -9,7 +9,7 @@ export default function Home() {
     backgroundSize: 'cover',
     
     [theme.breakpoints.up('xs')]: {
-      backgroundPosition: '600px 54px'
+      backgroundPosition: '75% 56px'
     },
     [theme.breakpoints.up('sm')]: {
       backgroundPosition: '0px 54px'
@@ -23,16 +23,20 @@ export default function Home() {
     width: '100%',
     height: '100%',
     margin: 0,
+    [theme.breakpoints.up('sm')]: {
+      padding: '20px 10px'
+    },
   }));
 
   const PaperContainer = styled(Paper)(({ theme }) => ({
     backgroundColor: '#ffffffe8',
     ...theme.typography.body2,
-    padding: "40px 30px",
+    padding: "40px 30px 90px 30px",
     textAlign: 'left',
     color: theme.palette.text.secondary,
     borderRadius:'25px',
-    boxShadow: "9px 12px 20px -10px #2b2b2b"
+    boxShadow: "9px 12px 20px -10px #2b2b2b",
+    margin: "10px"
   }));
 
   const ButtonBig = styled(Button)(({ theme }) => ({
@@ -46,28 +50,35 @@ export default function Home() {
     fontSize: '14px'
   }));
 
+  const ButtonText = styled(Typography)(({ theme }) => ({
+    fontWeight: "bolder",
+    color: 'black',
+    fontSize: "21px",
+    textTransform: "none"
+  }));
+
   return (
     <BoxContainer height={"100vh"} >
       <BoxOverlay>
         <Container sx={{mt:10}} >
           <PaperContainer>
-            <Typography variant='h5' mb={4} fontWeight={'bolder'} color={'black'}>
+            <Typography variant='h4' mb={4} fontWeight={'bolder'} color={'black'}>
               Find people to do sports together
             </Typography>
             <Grid container spacing={2} >
               <Grid item xs={12} sm={6} >
                 <ButtonBig sx={{backgroundColor:"#FFD465"}}>
-                  <Typography variant='subtitle2' fontWeight={'bolder'} color={'black'}>
+                  <ButtonText>
                     I’m looking for...
-                  </Typography>
+                  </ButtonText>
                   <img src={Images.home.lookingFor} alt={'Looking for'}/>
                 </ButtonBig>
               </Grid>
               <Grid item xs={12} sm={6} >
                 <ButtonBig sx={{backgroundColor:"#00C6AE"}}>
-                  <Typography variant='subtitle2' fontWeight={'bolder'} color={'black'}>
+                  <ButtonText>
                     Look around
-                  </Typography>
+                  </ButtonText>
                   <img src={Images.home.lookAround} alt={'Look Around'} style={{marginTop: "8px", marginBottom: "2px"}}/>
                 </ButtonBig>
               </Grid>
